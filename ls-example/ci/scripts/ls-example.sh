2865b7305344c2a66a8cd68579fed17d116e216f
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2017-03-10 21:12:48 -0800

    changed file names

---
+++ ls-example/ci/scripts/ls-example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ls-example ls-example.sh
+
+set -e -x
+
+echo "List whats in the current directory"
+ls -lat 
---
+++ ls-example/update_concourse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# ls-example update_concourse.sh
+
+fly -t ci set-pipeline -p ls-example -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
