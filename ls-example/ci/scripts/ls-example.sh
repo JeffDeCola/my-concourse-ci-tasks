b91c8eca5fcc58864179fb3b36e8b22c34843434
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2017-03-11 09:10:36 -0800

    cleaned up

--- ls-example/ci/scripts/ls-example.sh
+++ ls-example/ci/scripts/ls-example.sh
@@ -5,17 +5,3 @@ set -e -x
 
 echo "List whats in the current directory"
 ls -lat
-
-cd my-concourse-ci-tasks
-
-echo "hi jeff" > boo.txt
-
-ls -lat
-
-cd ..
-
-cd result
-
-echo "hi jeff" > blah.txt
-
-ls -lat
